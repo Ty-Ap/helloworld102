f5b3822913bfbeafd8ce4f257b85dcb73de6f367
Ty Aponte <aaustinsk8@aol.com>
2022-11-17 13:05:24 -0800

    functions added and transferred into .js

---
+++ script.js
@@ -0,0 +1,19 @@
+
+alert ("lorem ipsum");
+
+function enterName() {
+    let username=prompt("what do i call you");
+    document.write("Hello " + username);
+    return username;
+}
+
+function math() {
+    let x=80
+    let y=340
+    let sum= x + y
+    document.write(sum + "blaze it");
+    return sum;
+
+}
+
+
